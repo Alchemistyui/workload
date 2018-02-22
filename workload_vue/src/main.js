6cb82531d9eb2082b0c106a410771dd2bd62dee0
Alchemist <1319525622@qq.com>
2018-02-22 17:05:50 +0800

    error

--- workload_vue/src/main.js
+++ workload_vue/src/main.js
@@ -3,8 +3,35 @@
 import Vue from 'vue'
 import App from './App'
 import 'lib-flexible'
+import Vue_Router from 'vue-router'
+import Vue_Resource from 'vue-resource'
+import routerMap from './routers.js'
+
+Vue.use(Vue_Router)
+Vue.use(Vue_Resource)
 
 Vue.config.productionTip = false
+
+// 请求编码转换
+Vue.http.options.emulateJSON = true
+
+var router = new Vue_Router({
+    hashbang: true,
+    history: false,
+    saveScrollPosition: false,
+    transitionOnLoad: true,
+    linkActiveClass: 'active'
+})
+
+// 全局的前置钩子函数，在路由切换开始时调用
+router.beforeEach(function(){
+    window.scrollTo(0,0)
+})
+
+// 独立出来的路由
+routerMap(router)
+
+router.start(App, '#app')
 
 /* eslint-disable no-new */
 new Vue({
