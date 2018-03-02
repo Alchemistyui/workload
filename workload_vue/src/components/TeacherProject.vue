de23d55477d6f9e42951404d1c58b18297a27351
Alchemist <1319525622@qq.com>
2018-03-02 22:02:15 +0800

    edit

--- workload_vue/src/components/TeacherProject.vue
+++ workload_vue/src/components/TeacherProject.vue
@@ -1,49 +1,77 @@
 <template>
     <div>
-        <select v-model="selectState" id="state">
+        <select v-model="searchType" id="state">
             <option disabled value="">项目状态</option>
             <option v-for="state in states">{{state.text}}</option>
         </select>
 
 
         <div>
-            <input type="text" placeholder="教师姓名" @click="showTeacher" v-model="searchTeacher">
-            <table>
-                 <tr v-for="item in filterTeachers">{{item}}</tr>
-            </table>
-        </div>
+            <input type="text" placeholder="教师姓名" @blur="showTeTable = false" @click="showTeacher" v-model="searchTeacher">
+            <table v-if="showTeTable"> 
+             <tr v-for="item in filterTeachers">{{item}}</tr>
+         </table>
+     </div>
+
+
+     <div>
+        <input type="text" placeholder="规则名称" @blur="showRuTable = false" @click="showRule" v-model="searchRule">
+        <table v-if="showRuTable">
+         <tr v-for="item in filterRules">{{item}}</tr>
+     </table>
+ </div>
+
+ <button @click="search">搜索</button>
+
+
+
+ <p>每页
+    <select v-model="limit" id="state">
+        <option>1</option>
+        <option>3</option>
+        <option>5</option>
+    </select>
+条</p>
 
 
 
 
-        <button>导出</button>
+<button>导出</button>
 
-        <table class="table table-striped">
-            <tr>
-                <th>序号</th>
-                <th>规则名称</th>
-                <th>类别</th>
-                <th>项目名称</th>
-                <th>申报形式</th>
-                <th>工作当量</th>
-                <th>教师姓名</th>
-                <th>项目状态</th>
-                <th>操作</th>
-            </tr>
-            <tr v-for="(item, index) in reverseItems">
-                <td>{{index + 1}}</td>
-                <td>{{item.ruleName}}</td>
-                <td>{{item.type}}</td>
-                <td>{{item.projectName}}</td>
-                <td>{{item.form}}</td>
-                <td>{{item.workload}}</td>
-                <td>{{item.teacher}}</td>
-                <td>{{item.projectState}}</td>
-                <td><button>退回导入</button><button>退回复核</button><button>查看详情</button></td>
-            </tr>
-        </table>
+<table class="table table-striped">
+    <tr>
+        <th>序号</th>
+        <th>规则名称</th>
+        <th>类别</th>
+        <th>项目名称</th>
+        <th>申报形式</th>
+        <th>工作当量</th>
+        <th>教师姓名</th>
+        <th>项目状态</th>
+        <th>操作</th>
+    </tr>
+    <tr v-for="(item, index) in paginate">
+        <td>{{index + 1}}</td>
+        <td>{{item.ruleName}}</td>
+        <td>{{item.type}}</td>
+        <td>{{item.projectName}}</td>
+        <td>{{item.form}}</td>
+        <td>{{item.workload}}</td>
+        <td>{{item.teacher}}</td>
+        <td>{{item.projectState}}</td>
+        <td><button>退回导入</button><button>退回复核</button><button>查看详情</button></td>
+    </tr>
+</table>
 
-    </div>
+<p>共{{lists.length}}条当前工作目录</p>
+
+<p class="pagination">
+    <a class="button" @click="changePage(-1)">上一页</a>
+    当前第<input v-model="page" type="text" class="page">页 共{{ total }}页
+    <a class="button" @click="changePage(1)">下一页</a>
+</p>
+
+</div>
 </template>
 
 
@@ -52,9 +80,17 @@
 export default {
   data () {
     return {
-        selectState: '',
+        searchType: '',
         searchTeacher: '',
+        searchRule: '',
+        showTeTable: false,
+        showRuTable: false,
         teachers: [],
+        rules: [],
+        filteredArray: [],
+        total: 0,
+        limit: 3,
+        page: 1,
         states: [
         {text: '待审核/待复核'},
         {text: '审核/复核已通过'},
@@ -81,7 +117,7 @@ computed: {
         var teachers = this.teachers;
         return teachers.filter(function (item) {
             return item.indexOf(searchTeacher) !== -1
-         })
+        })
 
 
             // var self = this
@@ -89,8 +125,36 @@ computed: {
             //   var searchRegex = new RegExp(self.searchQuery, 'i')
             //   return searchRegex.test(teacher)
             // })
+        },
+        filterRules: function () {
+            var searchRule = this.searchRule;
+            var rules = this.rules;
+            return rules.filter(function (item) {
+                return item.indexOf(searchRule) !== -1
+            })
+        },
+        paginate () {
+            var arr = [];
+            // console.log('paginate')
+            // var searchRule = this.searchRule;
+            // var searchTeacher = this.searchTeacher;
+            // var searchType = this.searchType;
+            // var reverseItems = this.reverseItems;
+            // arr = reverseItems.filter(function (item) {
+            //     return item.indexOf(searchTeacher) !== -1 && item.indexOf(searchRule) !== -1 && item.indexOf(searchType) !== -1
+            // },
+            this.total = Math.ceil(this.reverseItems.length / this.limit)
+            let page = parseInt(this.page - 1)
+            if (page < 0) {
+                page = 0
             }
-    
+
+            
+            // alert(arr.push)
+            arr = this.reverseItems.slice(this.limit * page, this.limit * (page + 1))
+            return arr   
+        }        
+
             // teacher: function () {
 
                 // var result = [], hash = {};
@@ -112,7 +176,7 @@ computed: {
         },
         methods: {
             // 搜索
-            search(e) {
+            // search(e) {
 
                 // var v = e.target.value,
                 //     self = this;
@@ -135,21 +199,70 @@ computed: {
                 //     // 没有搜索内容，则展示全部数据
                 //     this.teacher = ss;
                 // }
-            },
+            // },
             showTeacher() {
                 var hash = {};
+                // var len = this.lists.length;
+                // alert(len)
                 // alert(this.lists[0].teacher)
                            // alert(this.filterTeachers)
-                for (var i = 0, elem; (elem = this.lists[i].teacher) != null; i++) {
+                           var arr = [];
+                           for (var i = 0, elem; i < this.lists.length; i++) {
+                            elem = this.lists[i].teacher;
                     // alert(elem + i)
                     if (!hash[elem]) {
-                        this.teachers.push(elem);
+                        arr.push(elem);
                         hash[elem] = true;
                     }
-                }
+                // alert(arr);
+            };
+                // alert(arr);
+                this.teachers = arr;
+                this.showTeTable = true
+            },
+            showRule() {
+                var hash = {};
+                var arr = [];
+                           // alert(this.filterTeachers)
+                           for (var i = 0, elem; i < this.lists.length; i++) {
+                            elem = this.lists[i].ruleName;
+                    // alert(elem + i)
+                    if (!hash[elem]) {
+                        arr.push(elem);
+                        hash[elem] = true;
+                    }
+                };
+                this.rules = arr;
+                this.showRuTable = true
+            },
+            changePage (num) {
+              if (num === 1) {
+                if (this.page < this.total) {
+                  this.page++
+              } else {
+                  window.alert('已是最后一页')
+              }
+          } else {
+            if (this.page > 1) {
+              this.page--
+          } else {
+              window.alert('已是第一页')
+          }
+      }
+  },
+  // 取不到过滤后的数组啊我的天
+  search() {
+    var arr = [];
+    var searchRule = this.searchRule;
+    var searchTeacher = this.searchTeacher;
+    var searchType = this.searchType;
+    var reverseItems = this.reverseItems;
+    arr = reverseItems.filter(function (item) {
+        return item.teacher.indexOf(searchTeacher) !== -1 && item.ruleName.indexOf(searchRule) !== -1 && item.type.indexOf(searchType) !== -1
+    })
+    alert(arr)
+}
+}
+}
 
-
-            }
-        }
-    }
-    </script>
+</script>
